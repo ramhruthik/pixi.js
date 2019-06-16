bae5b9d4560cc0649831b280f376ecde0e0201e4
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2019-06-16 20:08:07 +0300

    make createIndicesForQuads public, add uint32

--- packages/utils/src/data/createIndicesForQuads.js
+++ packages/utils/src/data/createIndicesForQuads.js
@@ -1,19 +1,19 @@
 /**
  * Generic Mask Stack data structure
  *
- * @memberof PIXI
+ * @memberof PIXI.utils
  * @function createIndicesForQuads
- * @private
  * @param {number} size - Number of quads
- * @return {Uint16Array} indices
+ * @param {boolean} use32bit - use 32-bit indices
+ * @return {Uint16Array|Uint32Array} indices, 6 times number of quads
  */
-export function createIndicesForQuads(size)
+export function createIndicesForQuads(size, use32bit)
 {
     // the total number of indices in our array, there are 6 points per quad.
 
     const totalIndices = size * 6;
 
-    const indices = new Uint16Array(totalIndices);
+    const indices = use32bit ? new Uint32Array(totalIndices) : new Uint16Array(totalIndices);
 
     // fill the indices with the quads to draw
     for (let i = 0, j = 0; i < totalIndices; i += 6, j += 4)
